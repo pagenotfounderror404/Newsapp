bcc6e00b4c9a5bcdf1ffe97acd1657eabfc8ba21
Ayush Chakraborty <Ayush.Chakraborty@gds.ey.com>
2023-12-28 04:30:50 +0530

    Revert "Updated submodule"
    This reverts commit f76e0a5892cf0680673c16f6090221e16c0b1ce9.

--- src/App.js
+++ src/App.js
@@ -1,113 +1,25 @@
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
-import React, { useState } from 'react'
-import Navbar from './Components/Navbar'
-import News from '../../newsapp/src/Components/News'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LoadingBar from 'react-top-loading-bar'
-
-const App = () => {
-  const [progress, setProgress] = useState(0)
+function App() {
   return (
-    <Router>
-      <LoadingBar height="5px" color="red" progress={progress} />
-      <div>
-        <Navbar />
-      </div>
-      <Routes>
-        <Route exact path="/About"></Route>
-        <Route
-          exact
-          path="/"
-          element={<News setProgress={setProgress} key="general" />}
-        ></Route>
-        <Route
-          exact
-          path="/Business"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="business"
-              key="business"
-            />
-          }
-        ></Route>
-        <Route
-          exact
-          path="/Science"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="science"
-              key="science"
-            />
-          }
-        ></Route>
-        <Route
-          exact
-          path="/Sports"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="sports"
-              key="sports"
-            />
-          }
-        ></Route>
-        <Route
-          exact
-          path="/Technology"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="technology"
-              key="technology"
-            />
-          }
-        ></Route>
-        <Route
-          exact
-          path="/General"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="general"
-              key="general"
-            />
-          }
-        ></Route>
-        <Route
-          exact
-          path="/Entertainment"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="entertainment"
-              key="entertainment"
-            />
-          }
-        ></Route>
-        <Route
-          exact
-          path="/Health"
-          element={
-            <News
-              setProgress={setProgress}
-              country="in"
-              category="health"
-              key="health"
-            />
-          }
-        ></Route>
-      </Routes>
-    </Router>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
